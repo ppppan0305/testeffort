d57868b7f36648b6204a1bfb5c90852fac97ca03
pppan <635645048@qq.com>
2020-12-03 13:59:47 +0800

    sdf

--- src/main/java/TestEffort_1.java
+++ src/main/java/TestEffort_1.java
@@ -1,11 +1,17 @@
 //import org.springframework.web.util.WebUtils;
-import org.springframework.http.ContentDisposition;
+//import org.springframework.http.ContentDisposition;
+import org.springframework.web.util.WebUtils;
+
+
 public class TestEffort_1 {
 
     public static void main(String args[]){
 //       System.out.println(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
-        ContentDisposition.Builder a = ContentDisposition.builder("sda");
-        System.out.println(a.filename("asdad"));
+//        ContentDisposition.Builder a = ContentDisposition.builder("sda");
+//        System.out.println(a.filename("asdad"));
+
+        WebUtils.parseMatrixVariables("asdfasdfwaef");
+
 
 
     }
