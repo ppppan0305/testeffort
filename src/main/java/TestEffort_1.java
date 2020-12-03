b3394b64fd3269b7e87c98a98f8e53f56a1ecc34
pppan <635645048@qq.com>
2020-12-03 13:55:40 +0800

    xf

--- src/main/java/TestEffort_1.java
+++ src/main/java/TestEffort_1.java
@@ -1,12 +1,14 @@
-import org.springframework.web.util.WebUtils;
+//import org.springframework.web.util.WebUtils;
+import org.springframework.http.ContentDisposition;
 public class TestEffort_1 {
 
     public static void main(String args[]){
-       System.out.println(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
-    }
-}
-class D extends WebUtils{
+//       System.out.println(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
+        ContentDisposition.Builder a = ContentDisposition.builder("sda");
+        System.out.println(a.filename("asdad"));
 
+
+    }
 }
 
 
