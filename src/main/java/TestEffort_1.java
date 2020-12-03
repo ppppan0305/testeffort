c5f08836d9cc2b464dd422194c1a7885e0efb276
pppan <635645048@qq.com>
2020-12-03 11:01:40 +0800

    xsfdf

--- src/main/java/TestEffort_1.java
+++ src/main/java/TestEffort_1.java
@@ -1,11 +1,13 @@
-
+import org.springframework.web.util.WebUtils;
 public class TestEffort_1 {
 
     public static void main(String args[]){
-       
+       System.out.println(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
     }
 }
+class D extends WebUtils{
 
+}
 
 
 
