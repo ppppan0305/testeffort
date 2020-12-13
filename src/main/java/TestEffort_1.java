d4f745a969cab2c84259f1f1d158150ec5baeb1a
pppan <635645048@qq.com>
2020-12-13 16:02:03 +0800

    hadoop common 2.6.0

--- src/main/java/TestEffort_1.java
+++ src/main/java/TestEffort_1.java
@@ -1,17 +1,28 @@
 //import org.springframework.web.util.WebUtils;
 //import org.springframework.http.ContentDisposition;
-import org.springframework.web.util.WebUtils;
-
+//import org.springframework.web.util.WebUtils;
+//import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+//import org.springframework.oxm.mime.MimeContainer;
+//import org.springframework.oxm.support.AbstractMarshaller;
+//
+//
 
 public class TestEffort_1 {
 
     public static void main(String args[]){
 //       System.out.println(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
+//
 //        ContentDisposition.Builder a = ContentDisposition.builder("sda");
 //        System.out.println(a.filename("asdad"));
+//
+//        WebUtils.parseMatrixVariables("asdfasdfwaef");
+//
 
-        WebUtils.parseMatrixVariables("asdfasdfwaef");
 
+//        Jaxb2Marshaller j = new Jaxb2Marshaller();
+//        j.unmarshal("", MimeContainer);
+
+//        AbstractMarshaller a = new AbstractMarshaller();
 
 
     }
--- src/main/java/pan/ppan/pppan/Test/Test.java
+++ src/main/java/pan/ppan/pppan/Test/Test.java
@@ -1,18 +1,17 @@
 package pan.ppan.pppan.Test;
 
-//import org.apache.hadoop.fs.FileSystem;
-//import org.apache.hadoop.fs.Path;
-//import org.kitesdk.data.spi.DefaultConfiguration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
 
 public class Test {
 //
-//    public void tttest(String ar) throws IOException {
-//        Path schemaPath = new Path("hdfs:/tmp/schema.avsc");
-//        System.out.println(ar);
-//        FileSystem fs = schemaPath.getFileSystem(DefaultConfiguration.get());
-//    }
+    public void tttest(String ar) throws IOException {
+        Path schemaPath = new Path("hdfs:/tmp/schema.avsc");
+        System.out.println(ar);
+        FileSystem fs = schemaPath.getFileSystem(null);
+    }
 
     public static void main(String args[]){
 
